a415b8e15c141278c51d2f776e321d448782ca1b
animate-object <alexander.f.prinsen@gmail.com>
2019-11-29 21:55:03 -0600

    startswith

--- src/main.py
+++ src/main.py
@@ -46,14 +46,22 @@ def spellable_result(result_list: Words, letters: Letters, bounds: Bounds):
     )
 
 
-def match_result(result_dict: Dict, substringBounds: Bounds, bounds: Bounds):
+def starts_with_result(result_list: Words, starts_with: str, bounds: Bounds):
+    return dict(
+        result=dict(items=result_list, total=len(result_list)),
+        query=dict(startsWith=starts_with, minLength=bounds.getMin(),
+                   maxLength=bounds.getMax())
+    )
+
+
+def match_result(result_dict: Dict, substring_bounds: Bounds, bounds: Bounds):
     return dict(
         result=dict(items=result_dict, total=len(result_dict)),
         query=dict(
             minLength=bounds.getMin(),
             maxLength=bounds.getMax(),
-            start=substringBounds.getMin(),
-            end=substringBounds.getMax()
+            start=substring_bounds.getMin(),
+            end=substring_bounds.getMax()
         )
     )
 
@@ -84,17 +92,32 @@ def query_database_for_combinations(search_combinations: List[str]) -> Words:
     return result
 
 
+def query_database_for_words_starting_with(starts_with, bounds): Words:
+    result = []
+    with get_db_connection().cursor() as cur:
+        cur.execute(f"""
+            SELECT * 
+                FROM `word`
+                WHERE LENGTH(`ordered`) > {bounds.getMin() - 1}
+                AND LENGTH(`ordered`) < {bounds.getMax() + 1}
+                AND `ordered` LIKE '{starts_with}%';
+        """)
+        for row in cur:
+            result.append(row[0])
+    return result
+
+
 def query_database_match_substrings(substringBounds, bounds) -> Dict:
     result = {}
     subMin = substringBounds.getMin() + 1
     subMax = substringBounds.getMax()
     with get_db_connection().cursor() as cur:
         cur.execute(f"""
-        SELECT substring(`ordered`, {subMin}, {subMax}), COUNT(*)
-            FROM `word`
-            WHERE LENGTH(`ordered`) > {bounds.getMin() - 1}
-            AND LENGTH(`ordered`) < {bounds.getMax() + 1}
-            GROUP BY substring(`ordered`, {subMin}, {subMax});
+            SELECT substring(`ordered`, {subMin}, {subMax}), COUNT(*)
+                FROM `word`
+                WHERE LENGTH(`ordered`) > {bounds.getMin() - 1}
+                AND LENGTH(`ordered`) < {bounds.getMax() + 1}
+                GROUP BY substring(`ordered`, {subMin}, {subMax});
         """)
         for row in cur:
             result[row[0]] = row[1]
@@ -122,6 +145,32 @@ def parse_spellable_query(query: Dict) -> (Letters, Bounds):
                 400, f"Invalid minLength arg, minLength must be between 1 and 7")
 
         return letters, Bounds(min_, max_)
+    except:
+        raise LambdaStatusException(
+            400, f"Error parsing query payload {json.dumps(query)}.")
+
+
+def parse_starts_with_query(query: Dict) -> (Letters, Bounds):
+    try:
+        starts_with_arg = query['starts_with']
+        if any([not l.isalpha() for l in starts_with_arg]) or len(starts_with_arg) > 5:
+            raise LambdaStatusException(400,
+                                        f"Invalid arg starts_with: {starts_with_arg}. Letters must be a member of the english alphabet. Max 5 letters allowed")
+        starts_with = ''.join(sorted(starts_with_arg.lower()))
+
+        max_arg = str(query.get('maxLength'))
+        min_arg = str(query.get('minLength'))
+        max_ = int(max_arg) if max_arg.isdigit() else 7
+        min_ = int(min_arg) if min_arg.isdigit() else 1
+
+        if (max_ > 7 or max_ < 1):
+            raise LambdaStatusException(
+                400, f"Invalid maxLength arg, maxLength must be between 1 and 7")
+        elif (min_ > 7 or min_ < 1):
+            raise LambdaStatusException(
+                400, f"Invalid minLength arg, minLength must be between 1 and 7")
+
+        return starts_with, Bounds(min_, max_)
     except:
         raise LambdaStatusException(
             400, f"Error parsing query payload {json.dumps(query)}.")
@@ -169,9 +218,13 @@ def handle(event, context):
             data = get_spellable_words_for_letters(letters, bounds)
             return response(200, spellable_result(data, letters, bounds))
         elif (queryType == 'matchSubstring'):
-            substringBounds, bounds = parse_match_substring_query(query)
-            data = query_database_match_substrings(substringBounds, bounds)
-            return response(200, match_result(data, substringBounds, bounds))
+            substring_bounds, bounds = parse_match_substring_query(query)
+            data = query_database_match_substrings(substring_bounds, bounds)
+            return response(200, match_result(data, substring_bounds, bounds))
+        elif (queryType == 'startsWith'):
+            starts_with, bounds = parse_starts_with_query(query)
+            data = query_database_for_words_starting_with(starts_with, bounds)
+            return response(200, starts_with_result(data, starts_with, bounds))
 
         raise LambdaStatusException(
             400, f"Error parsing event -- invalid query {query}")
